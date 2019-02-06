49604b3cd842316258d27d5fedaca34159c4de10
pkijak <46262684+pkijak@users.noreply.github.com>
2019-02-06 13:18:36 +0100

    wstepne menu

--- src/com/resellnote/Main.java
+++ src/com/resellnote/Main.java
@@ -8,8 +8,43 @@ public class Main {
     private static String commands = "[-1]QUIT, [0]Polecenia, [1]Dostepne przedmioty, [2]Sprzedane przedmioty, [3]Dodaj przedmiot, [4]Usun przedmiot,"
             + " [5]Edytuj przedmiot, [6]Sprzedaj. [7] Bilans";
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
+        System.out.println("|   RESELL NOTE    |");
         System.out.println(commands);
+
+        boolean flag = true;
+        while (flag) {
+            System.out.println("Wprowadz polecenie: ");
+            
+
+            int answer = in.nextInt();
+            switch (answer) {
+
+                case -1:
+                    System.out.println("QUIT");
+                    flag = false;
+                    break;
+
+                case 0:
+                    System.out.println(commands);
+                    break;
+                case 1:
+                    // dostepne itemy
+                case 2:
+                    // sprzedane itemy
+                case 3:
+                    // dodaj item
+                case 4:
+                    // usun item
+                case 5:
+                    // edytuj item
+                case 6:
+                    // sprzedaj item
+                case 7:
+                    // bilans
+
+            }
+        }
     }
 }
